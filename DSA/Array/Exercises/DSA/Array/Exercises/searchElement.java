c5a64e1051900d413481e8a824e92559defc48ad
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-06-24 12:32:24 +0530

    searchElement_array

--- DSA/Array/Exercises/DSA/Array/Exercises/searchElement.java
+++ DSA/Array/Exercises/DSA/Array/Exercises/searchElement.java
@@ -1,7 +1,34 @@
 package DSA.Array.Exercises;
 
+import java.util.Scanner;
+
 public class searchElement {
+
+    public static boolean searchEle(int[] arr, int num)
+    {
+        for (int i = 0; i < arr.length; i++)
+            {
+                if(num == arr[i])
+                {
+                    return true;
+                }
+            }
+            return false;
+    }
+
     public static void main(String[] args) {
-        
+         try (Scanner sc = new Scanner(System.in)) {
+            int num = 20;
+            System.out.println("Add size of array: ");
+            int n = sc.nextInt();
+
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+
+            System.out.println(searchEle(arr, num));
+            System.out.println(searchEle(arr, 2));
+        }
     }
 }
