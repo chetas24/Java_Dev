1613816ba5d7a0decab66268cbd4a7247ea7354d
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-06-29 19:03:16 +0530

    Ceiling and Floor

---
+++ DSA/Array/BinarySearch/floor.java
@@ -0,0 +1,49 @@
+package DSA.Array.BinarySearch;
+
+public class floor {
+    // floor of a number
+    public static int binarySearch(int[] arr, int k)
+    {
+        if (arr == null || arr.length <= 0) {
+            System.out.println("Array is empty");
+            return k;
+        }
+
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (k < arr[mid]) {
+                end = mid - 1;
+            }
+            else if (k > arr[mid])
+            {
+                start = mid + 1;
+            }
+            else{
+            return mid;
+            }
+        }
+        return end;
+    }
+
+
+    public static void display(int[] arr)
+    {
+        for (int i = 0; i < arr.length; i++)
+        {
+            System.out.print(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 3, 5, 9, 14, 16, 18};
+        int target = 10;
+
+        display(arr);
+        System.out.println();
+        int result = binarySearch(arr, target);
+        System.out.println(result);
+    }
+}
