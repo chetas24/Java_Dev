447d695ea6e45c785353aac3da8c3aae41269de1
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2024-07-06 18:24:34 +0530

    ....

---
+++ DSA/Array/Practice/noofemployees.java
@@ -0,0 +1,22 @@
+package DSA.Array.Practice;
+
+public class noofemployees {
+
+    public static int numberOfEmployeesWhoMetTarget(int[] hours, int target) {
+        int count = 0;
+        for(int i = 0; i < hours.length; i++)
+        {
+            if(hours[i] >= target)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int[] hours = {0,1,2,3,4};
+        int target = 3;
+        int result = numberOfEmployeesWhoMetTarget(hours, target);
+        System.out.println(result);
+    }
+}
