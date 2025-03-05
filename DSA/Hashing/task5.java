b6d3095df6320c899b17de51336a29d7791c0e9c
Chetas Barhate <23421818+chetas24@users.noreply.github.com>
2025-03-05 19:38:49 +0530

    Hashing Implementation

---
+++ DSA/Hashing/task5.java
@@ -0,0 +1,7 @@
+package DSA.Hashing;
+//
+public class task5 {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ DSA/Hashing/task6impl.java
@@ -0,0 +1,20 @@
+package DSA.Hashing;
+
+class Node<K, V>{
+    K key;
+    V value;
+    Node<K, V> next; // for collision
+
+    public Node(K key, V value)
+    {
+        this.key = key;
+        this.value = value;
+        this.next = null;
+    }
+}
+
+public class task6impl {
+    public static void main(String[] args) {
+        
+    }
+}
